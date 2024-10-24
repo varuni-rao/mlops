c45906abb910afacee20a7c90efbabe67252a800
varuni-rao <154630270+varuni-rao@users.noreply.github.com>
2024-10-24 12:03:19 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,8 +27,8 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-#RESOURCE_GROUP_NAME="cloudml"
-GROUP="default_resource_group"
+RESOURCE_GROUP_NAME="default_resource_group"
+GROUP="testml"
 LOCATION="eastus"
 WORKSPACE="carmpg" #"test_workspace_azure_ml"
 # </set_variables>
